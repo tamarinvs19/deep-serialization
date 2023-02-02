40bfdfc573a6eb15f65488a2d2e0b66cf6ab38a8
Vyacheslav Tamarin <vyacheslav.tamarin@yandex.ru>
2023-02-02 09:42:44 +0300

    Remove unused function

--- deep_serialization/deep_serialization.py
+++ deep_serialization/deep_serialization.py
@@ -29,16 +29,3 @@ def serialize_objects(objs: List[Any]) -> Tuple[List[str], str]:
     ]
     return ids, json.dumps({'objects': serializer.memory}, cls=MemoryDumpEncoder)
 
-
-def serialize_to_json_objects(objs: List[Any]) -> Tuple[List[str], str]:
-    """
-    Serialize objects with shared memory.
-    Returns list of object ids and memory dump.
-    """
-
-    serializer = PythonSerializer()
-    ids = [
-        serializer.write_object_to_memory(obj)
-        for obj in objs
-    ]
-    return ids, MemoryDumpEncoder().default({'objects': serializer.memory})
